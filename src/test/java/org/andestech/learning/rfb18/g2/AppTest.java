f7da8d9e8e471f406b370f897c0e5d003ac1c863
a0z9 <a0z9@rambler.ru>
2019-02-23 10:29:36 +0300

    1

---
+++ src/test/java/org/andestech/learning/rfb18/g2/AppTest.java
@@ -0,0 +1,18 @@
+package org.andestech.learning.rfb18.g2;
+
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+
+
+public class AppTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+
+        assertTrue( true );
+
+
+    }
+}
