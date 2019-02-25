86e310b284d869802c7801d81b760c9cc54d1b3d
a0z9 <a0z9@rambler.ru>
2019-02-25 18:57:53 +0300

    4

--- src/test/java/org/andestech/learning/rfb18/g2/AppDataTest.java
+++ src/test/java/org/andestech/learning/rfb18/g2/AppDataTest.java
@@ -1,6 +1,7 @@
 package org.andestech.learning.rfb18.g2;
 
 import org.testng.Assert;
+import org.testng.ITestContext;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -40,7 +41,14 @@ public class AppDataTest {
     }
 
 
-    @Test(dataProvider = "logindata2")
+    @DataProvider(name = "logindata3")
+    public static Object[][] getLoginDatafromFile2(ITestContext ctx){
+        String fname = ctx.getCurrentXmlTest().getParameter("fileName");
+
+        return Utils.getLoginsfromDisk(fname);
+    }
+
+    @Test(dataProvider = "logindata3")
     public void massLoginTest(String name, String sname, String expectedLogin  ){
 
 //        String name = "Vasily";
